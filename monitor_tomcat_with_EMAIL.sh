b4faab6051f8a09b41fc8535918ea0916e3be623
Merlin <loolwv7@gmail.com>
2013-08-08 11:36:14 +0800

    Optimized something

--- monitor_tomcat_with_EMAIL.sh
+++ monitor_tomcat_with_EMAIL.sh
@@ -21,21 +21,20 @@ fi
 
 SERVER_IP="www.zjpy.gov.cn"
 KEYWORD="走进平阳"
-TEST=$(curl ${SERVER_IP} 2>&1 | grep ${KEYWORD} | awk -F">" '{ print $3 }' | awk -F"<" '{ print $1 }')
-OOME=$(tail -20 ${LOGFILE} | egrep -m 1 -oh -i 'OutOfMemoryError|to prevent a memory leak')
-#OOME=$(grep -m 1 -oh -i OutOfMemoryError /var/log/tomcat/catalina.`date +%F`.log)
+TEST=$(curl ${SERVER_IP} 2>&1 | grep -m 1 -oh ${KEYWORD})
+OOME=$(tail -100 ${LOGFILE} | egrep -m 1 -oh -i 'OutOfMemoryError|to prevent a memory leak')
 
-if  [ ! -z $TEST ]; then 
+if  [ ! -z '$TEST' ]; then 
 	echo " Tomcat is OK. `date +%F_%T`" >> /tmp/tomcat_status.log
 else
 	echo "Warning!!! Tomcat isn't running?? Please CHECK IT IMMEDIATELY!!! `date +%F_%T`" >> /tmp/tomcat_status.log
-	echo "警告，服务器$SERVER_IP运行不正常，请立刻检查!  `date +%F_%T`" | mutt -s "温州市平阳县电子政务服务器网站警告!" loolwv7@gmail.com
+	echo "警告，服务器'$SERVER_IP'运行不正常，请立刻检查!  `date +%F_%T`" | mutt -s "温州市平阳县电子政务服务器网站警告!" loolwv7@gmail.com
 
-	if [ ! -z $OOME ]; then 
+	if [ ! -z '$OOME' ]; then 
 		/etc/init.d/tomcat restart
 	sleep 33
                 echo "" > ${LOGFILE}
-		echo "注意，报$OOME错误! 已尝试自动重启tomcat服务，请检查服务器$SERVER_IP的情况!!!  `date +%F_%T`" >> /tmp/tomcat_status.log
-		echo "注意，报$OOME错误! 已尝试自动重启tomcat服务，请检查服务器$SERVER_IP的情况!!!  `date +%F_%T`" | mutt -s "温州市平阳县电子政务服务器网站错误!" loolwv7@gmail.com
+		echo "注意，报'$OOME错误'! 已尝试自动重启tomcat服务，请检查服务器'$SERVER_IP'的情况!!!  `date +%F_%T`" >> /tmp/tomcat_status.log
+		echo "注意，报'$OOME错误'! 已尝试自动重启tomcat服务，请检查服务器'$SERVER_IP'的情况!!!  `date +%F_%T`" | mutt -s "温州市平阳县电子政务服务器网站错误!" loolwv7@gmail.com
 	fi
 fi
